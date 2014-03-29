08f82c3c7ef1d29e633315cd9240c88209f5330a
Elliot <ere16@case.edu>
2014-03-28 22:37:59 -0400

    Started Troop class

---
+++ src/com/etcz/codebrawl/Troop.java
@@ -0,0 +1,23 @@
+package com.etcz.codebrawl;
+
+public class Troop {
+	private float x;
+	private float y;
+	private int health;
+	private static int radius;
+	
+	public float getX(){
+		return x;
+	}
+	public float getY(){
+		return y;
+	}
+	
+	public final void walk(){
+		//creates instance of gameturn
+	}
+	
+	public final void shoot(){
+		//creates instance of gameturn
+	}
+}
