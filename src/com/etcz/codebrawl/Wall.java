23cf4f16098128be57a58eb2ff246d73cb2eef0a
Zac Audette <zac@Zacs-MacBook-Air.local>
2014-03-28 23:08:22 -0400

    Wall.java

---
+++ src/com/etcz/codebrawl/Wall.java
@@ -0,0 +1,13 @@
+package com.etcz.codebrawl;
+
+public class Wall {
+
+    private int x;
+    private int y;
+
+    public Wall(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
