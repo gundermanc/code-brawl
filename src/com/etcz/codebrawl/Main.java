415df99439cbe8d15472242629eeb481ca4a14e9
Elliot <ere16@case.edu>
2014-03-28 21:41:30 -0400

    yooo

--- src/com/etcz/codebrawl/Main.java
+++ src/com/etcz/codebrawl/Main.java
@@ -1,4 +1,4 @@
-package com.etcs.codebrawl;
+package com.etcz.codebrawl;
 
 import java.io.InputStream;
 import java.io.IOException;
@@ -7,5 +7,6 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
+    	System.out.print("yoo");
     }
 }
