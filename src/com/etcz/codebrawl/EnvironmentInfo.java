1d7db05f2d71b322417457bb7b5695ed6dc1bd91
Elliot <ere16@case.edu>
2014-03-29 01:08:52 -0400

    Environment info

---
+++ src/com/etcz/codebrawl/EnvironmentInfo.java
@@ -0,0 +1,32 @@
+package src.com.etcz.codebrawl;
+
+public class EnvironmentInfo
+{
+    public EnvironmentInfo()
+    {
+        super();
+    }
+    
+    private static double width;
+    private static double height;
+    private static final int MAX_TROOPS;
+    private Walls[] wall;
+    private Player[] players;
+    
+    public void setWalls()
+    {   
+    }
+    
+    private void setTroops()
+    {
+    }
+    
+    public void setPlayers()
+    {
+        
+    }
+    public void update(GameTurn turn)
+    {   
+    }
+    
+}
