2fb4bdc7829b28a8be3e6939442187429938019f
Zac Audette <zac@Zacs-MacBook-Air.local>
2014-03-29 14:50:00 -0400

    Added my (Zac) AI class as Player4

---
+++ src/com/etcz/codebrawl/Player4.java
@@ -0,0 +1,59 @@
+package com.etcz.codebrawl;
+
+import Random;
+
+public class Player4 extends Player {
+    
+    Troop me;
+    public void tick(Main.Troop troop) {
+        this.me = troop;
+        ArrayList<Troop> enemies;
+        ArrayList<Troop> temp = troop.look();
+
+        // grab all of the enemies
+        for (int i = 0; i < temp.size(); i++) {
+            if (troop.getPlayerID() != temp.get(i).getPlayerID()) {
+                enemies.add(temp.get(i));
+            }
+        }
+
+        // detect if an enemy is in range
+        Troop enemy = getNearestEnemy();
+        // shoot the enemy
+        if (enemy != null) {
+            this.me.shoot(enemy);
+        }
+        else {
+            // try and run away and don't make the same moves over and over
+            double rng = Math.random() * 4 + 1;
+            if (rng <= 1.0) {
+                // move right 1 and down 3
+                this.me.walk(1,3);
+            }
+            else if (rng > 1.0 && rng <= 2.0) {
+                // move right 3 and down 1
+                this.me.walk(3, 1);
+            }
+            else if (rng > 2.0 && rng <= 3.0) {
+                // move left 1 and up 3
+                this.me.walk(-1, -3);
+            }
+            else {
+                // move left 3 and up 1
+                this.me.walk(-3, -1);
+            }
+        }
+
+    }
+
+    // get the first enemy that is within your range
+    public Troop getNearestEnemy() {
+        for (int i = 0; i < enemies.size(); i++) {
+            if (Math.abs(this.me.getX() - enemies.get(i).getX() <= 30 || 
+                Math.abs(this.me.getY() - enemies.get(i).getY()) <= 30) {
+                return enemies.get(i);
+            }
+        }
+        return null;
+    }
+}
